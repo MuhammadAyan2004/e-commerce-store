4339d294735d46e208b0ef3595011765db965bfe
Muhammad AYAN <m101ayan92@gmail.com>
2024-11-07 23:30:04 +0500

    Create getCartProduct.js

---
+++ getCartProduct.js
@@ -0,0 +1,12 @@
+// import { updateCartValue } from "./updateCart";
+
+// export const getCartProductFromLS = ()=>{
+//     let cartProduct = localStorage.getItem("cartProductLS");
+
+//     if(!cartProduct){
+//         return [];
+//     }
+//     cartProduct = JSON.parse(cartProduct);
+//     updateCartValue(cartProduct);
+//     return cartProduct;
+// } 
