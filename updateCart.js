676f4e8d38e7877e5b259bfa0237b4d070a02752
Muhammad AYAN <m101ayan92@gmail.com>
2024-11-07 23:30:37 +0500

    Create updateCart.js

---
+++ updateCart.js
@@ -0,0 +1,6 @@
+// export const updateCartValue = (cartProduct)=>{
+// const cartvalue = document.querySelector(".cartvalue")
+
+//     return (cartvalue.innerHTML = `<i class="ri-shopping-cart-fill"></i>&nbsp;&nbsp;&nbsp;${cartProduct.length}</a>`)
+    
+// }
