a28adf81775f84efdd595b4355a02434bdf515c8
Muhammad AYAN <m101ayan92@gmail.com>
2024-11-13 17:27:32 +0500

    Update main.js

--- main.js
+++ main.js
@@ -2,5 +2,6 @@ import './style.css'
 import { showProductContainer } from "./homeProduct.js";
 
 import products from "./api/product.json";
-console.log(products);
+// console.log(products);
 showProductContainer(products)
+
