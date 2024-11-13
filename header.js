9bea2719d828e90ebe38298f43d50c6c48aeb40e
Muhammad AYAN <m101ayan92@gmail.com>
2024-11-13 17:27:21 +0500

    Create header.js

---
+++ header.js
@@ -0,0 +1,25 @@
+const header = document.querySelector(".headers")
+header.innerHTML =`    <div class="offer">
+        <p>Free shipping, 30 days return or refund guarantee</p>
+        <div class="log">
+            <a href="#">Log In</a>
+            <a href="#">Sign Up</a>
+        </div>
+    </div>
+    <nav>
+        <div class="logo">
+            <p class="sty">E-mart</p>
+            <p>Store</p>
+        </div>
+        <div class="links">
+            <a href="index.html" id="a">Home</a>
+            <a href="about.html" id="a">About</a>
+            <a href="products.html" id="a">Products</a>
+            <a href="contact.html" id="a">Contact</a>
+            <button type="button">
+                <a href="addToCart.html" class="cartvalue">
+                    <i class="ri-shopping-cart-fill"></i>&nbsp;&nbsp;&nbsp;0
+                </a>
+            </button>
+        </div>
+    </nav>`
