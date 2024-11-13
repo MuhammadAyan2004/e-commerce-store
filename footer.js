3b80003172e573c55f134fb3846cfe1a987d33ae
Muhammad AYAN <m101ayan92@gmail.com>
2024-11-13 17:27:15 +0500

    Create footer.js

---
+++ footer.js
@@ -0,0 +1,32 @@
+const footerElem = document.querySelector(".footer-section");
+footerElem.innerHTML = `<div class="container">
+        <div class="first">
+            <div class="logo change">
+                <p class="sty">E-mart</p>
+                <p>Store</p>
+            </div>
+            <p id="span">Welcome to E-Mart store, your ultimate destination for cutting-edges gadgets!</p>
+        </div>
+        <div class="sec">
+            <ul>SHOPPING
+
+            <li>Computer Store</li>
+            <li>Laptop store</li>
+            <li>Accessories</li>
+            <li>Sales & Discount</li>
+        </ul>
+        </div>
+        <div class="three">
+            <ul>EXPERIENCE
+
+            <li>Contact Us</li>
+            <li>Payment Method</li>
+            <li>Return & exchange</li>
+            <li>Delivery</li>
+        </ul>
+        </div>
+        <div class="fourth">
+            <ul>NEWSLETTER
+            <li>Be the first to know about arrivals, sales & promos</li></ul>
+        </div>
+    </div>`
