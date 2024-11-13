48b387b738fb303e823f07b043f5de645f1ca887
Muhammad AYAN <m101ayan92@gmail.com>
2024-11-13 17:27:53 +0500

    Update updateCart.js

--- updateCart.js
+++ updateCart.js
@@ -1,6 +1,9 @@
-// export const updateCartValue = (cartProduct)=>{
-// const cartvalue = document.querySelector(".cartvalue")
+const cartvalue = document.querySelector(".cartvalue");
 
-//     return (cartvalue.innerHTML = `<i class="ri-shopping-cart-fill"></i>&nbsp;&nbsp;&nbsp;${cartProduct.length}</a>`)
+export const updateCartValue = (cartProduct)=>{
+
+    if(cartvalue){
+        cartvalue.innerHTML=`<i class="ri-shopping-cart-fill"></i>&nbsp;&nbsp;&nbsp;${cartProduct.length}`;
+    }
     
-// }
+}
