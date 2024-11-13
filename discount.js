73557d90446ba3d139ae38ab9e150da928bc8124
Muhammad AYAN <m101ayan92@gmail.com>
2024-11-13 23:37:22 +0500

    Create discount.js

---
+++ discount.js
@@ -0,0 +1,53 @@
+const dis = document.querySelector(".megaDiscount");
+dis.innerHTML = `    <div class="discount">
+        <div class="dis-offer">
+            <div class="main-off">
+                <p>NEW YEAR SALE </p>
+                <h2>Get And Extra 50%</h2>
+                <a href="#">SHOW NOW</a>
+                    <img src="images/head.png" id="p-pic1">
+            </div>
+            <div class="main-off">
+                <p>NEW YEAR SALE </p>
+                <h2>Get And Extra 40%</h2>
+                <a href="#">SHOW NOW</a>
+                    <img src="images/watch.png" id="p-pic2">
+            </div>
+            <div class="main-off">
+                <p>NEW YEAR SALE </p>
+                <h2>Get And Extra 50%</h2>
+                <a href="#">SHOW NOW</a>
+                    <img src="images/laptop.png" id="p-pic3">
+            </div>
+        </div>
+        <div class="dis-offer-2">
+            <div class="main-on">
+                <i class="ri-truck-fill"></i>
+              <div>
+                <h5>Worldwide Shipping</h5>
+                <p>Order Above $100</p>
+              </div>
+            </div>
+            <div class="main-on">
+                <i class="ri-loop-right-line"></i>
+                <div>
+                    <h5>Worldwide Shipping</h5>
+                    <p>Order Above $100</p>
+                  </div>
+            </div>
+            <div class="main-on">
+                <i class="ri-money-dollar-box-line"></i>
+                <div>
+                    <h5>Worldwide Shipping</h5>
+                    <p>Order Above $100</p>
+                  </div>
+            </div>
+            <div class="main-on">
+                <i class="ri-customer-service-line"></i>
+                <div>
+                    <h5>Worldwide Shipping</h5>
+                    <p>Order Above $100</p>
+                  </div>
+            </div>
+        </div>
+    </div>`
