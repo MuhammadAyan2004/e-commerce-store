86749aad9597c7d8d94a0134360a50d157be0480
Muhammad AYAN <m101ayan92@gmail.com>
2024-11-13 20:55:23 +0500

    Create text.js

---
+++ text.js
@@ -0,0 +1,46 @@
+const about = document.querySelector(".ABOUT");
+about.innerHTML = `<div class="About">
+                <div class="abt">
+                    <h1>Why Choose E-mart Store</h1>
+                    <p>Choose E-mart EduHub for a holistic,enriching that empowers you to achive your goals.</p>
+                </div>
+                <div class="thought">
+                    <div class="tht1">
+                        <div class="thought1">
+                            <p id="edit">1</p>
+                            <h4 id="edit1">Wide selection</h4>
+                            <p>"Welcome to [Your Brand Name], your one-stop destination for all your shopping needs! Whether you're looking for the latest fashion trends, high-quality electronics, or everyday essentials, we’ve got you covered."</p>
+                        </div>
+                        <div class="thought1">
+                            <p id="edit">2</p>
+                            <h4 id="edit1">Quality Assurance</h4>
+                            <p>"Welcome to [Your Brand Name], your one-stop destination for all your shopping needs! Whether you're looking for the latest fashion trends, high-quality electronics, or everyday essentials, we’ve got you covered."</p>
+                        </div>
+                        <div class="thought1">
+                            <p id="edit">3</p>
+                            <h4 id="edit1">Competitive Prices</h4>
+                            <p>"Welcome to [Your Brand Name], your one-stop destination for all your shopping needs! Whether you're looking for the latest fashion trends, high-quality electronics, or everyday essentials, we’ve got you covered."</p>
+                        </div>
+                    </div>
+                    <div class="tht2">
+                        <div><img src="images/about.png"></div>
+                    </div>
+                    <div class="tht3">
+                        <div class="thought1">
+                            <p id="edit">4</p>
+                            <h4 id="edit1">Wide selection</h4>
+                            <p>"Welcome to [Your Brand Name], your one-stop destination for all your shopping needs! Whether you're looking for the latest fashion trends, high-quality electronics, or everyday essentials, we’ve got you covered."</p>
+                        </div>
+                        <div class="thought1">
+                            <p id="edit">5</p>
+                            <h4 id="edit1">Quality Assurance</h4>
+                            <p>"Welcome to [Your Brand Name], your one-stop destination for all your shopping needs! Whether you're looking for the latest fashion trends, high-quality electronics, or everyday essentials, we’ve got you covered."</p>
+                        </div>
+                        <div class="thought1">
+                            <p id="edit">6</p>
+                            <h4 id="edit1">Competitive Prices</h4>
+                            <p>"Welcome to [Your Brand Name], your one-stop destination for all your shopping needs! Whether you're looking for the latest fashion trends, high-quality electronics, or everyday essentials, we’ve got you covered."</p>
+                        </div>
+                    </div>
+                </div>
+    </div>`
